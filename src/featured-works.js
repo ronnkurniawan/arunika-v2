038a8b8e18c53b2c95a7db010c6db264ee7a0785
ronnkurniawan <ronnkurniawan@gmail.com>
2025-07-10 15:01:33 +0700

    horizontal scroll and mobile layout done

--- src/featured-works.js
+++ src/featured-works.js
@@ -5,33 +5,14 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const slideText = document.querySelector('#slide-text');
-
-const texts = [
-  'WORK PLAYGROUND',
-  'CREATIVE ADDICT',
-  'ART ENTHUSIAST',
-  'BRAND DESIGN'
-]
-
-function slideEffect () {
-  setInterval(() => {
-    slideText.textContent = texts[i];
-    i = (i + 1) % texts.length;
-  },800)
-
-}
-
-slideEffect();
-
 function lerp (start, end, p) {
   return start + (end - start) *  p
 }
 
 ScrollTrigger.create({
-  trigger: "#featured-works",
-  start: "top top",
-  end: "+=3000",
+  trigger: "#animate",
+  start: "bottom bottom",
+  end: "+=2000", // sama dengan 2000px
   scrub: true,
   pin: "#featured-works",
   markers: false,
@@ -45,7 +26,7 @@ ScrollTrigger.create({
 
     gsap.utils.toArray(".slide-img1").forEach((img, i) => {
       gsap.set(img, {
-        scale: lerp(0, 4, progress),
+        scale: lerp(0, 2, progress),
         y: `${lerp(20 + i, 40, progress)}vh`,
         x: `${lerp(30 + i, 40, progress)}vw`,
       });
@@ -53,7 +34,7 @@ ScrollTrigger.create({
 
     gsap.utils.toArray(".slide-img2").forEach((img, i) => {
       gsap.set(img, {
-        scale: lerp(0, 3, progress),
+        scale: lerp(0, 2, progress),
         x: `${lerp(-30 + i, 40, progress)}vw`,
         y: `${lerp(-20 + i, 40, progress)}vh`,
       })
@@ -61,9 +42,81 @@ ScrollTrigger.create({
 
     gsap.utils.toArray(".slide-img3").forEach((img, i) => {
       gsap.set(img, {
-        scale: lerp(0, 3, progress),
+        scale: lerp(0, 2, progress),
         x: `${lerp(20 + i, 40, progress)}vw`,
         y: `${lerp(-50 + i, 40, progress)}vh`
+      })
+    })
+
+    gsap.utils.toArray(".slide-img4").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1.5, progress),
+        y: `${lerp(20 + i, 40, progress)}vh`,
+        x: `${lerp(20 + i, 40, progress)}vw`,
+      });
+    });
+
+    gsap.utils.toArray(".slide-img5").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1.5, progress),
+        x: `${lerp(-55 + i, 40, progress)}vw`,
+        y: `${lerp(-50 + i, 40, progress)}vh`,
+      })
+    })
+
+    gsap.utils.toArray(".slide-img6").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1.5, progress),
+        x: `${lerp(-20 + i, 40, progress)}vw`,
+        y: `${lerp(-70 + i, 40, progress)}vh`
+      })
+    })
+
+    gsap.utils.toArray(".slide-img7").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1, progress),
+        x: `${lerp(40 + i, 40, progress)}vw`,
+        y: `${lerp(20 + i, 40, progress)}vh`
+      });
+    });
+
+    gsap.utils.toArray(".slide-img8").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1, progress),
+        x: `${lerp(-75 + i, 40, progress)}vw`,
+        y: `${lerp(-90 + i, 40, progress)}vh`,
+      })
+    })
+
+    gsap.utils.toArray(".slide-img9").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1, progress),
+        x: `${lerp(-20 + i, 40, progress)}vw`,
+        y: `${lerp(-120 + i, 40, progress)}vh`
+      })
+    })
+
+    gsap.utils.toArray(".slide-img10").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1, progress),
+        x: `${lerp(-110 + i, 40, progress)}vw`,
+        y: `${lerp(200 + i, 40, progress)}vh`
+      });
+    });
+
+    gsap.utils.toArray(".slide-img11").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1, progress),
+        x: `${lerp(-15 + i, 40, progress)}vw`,
+        y: `${lerp(-150 + i, 40, progress)}vh`,
+      })
+    })
+
+    gsap.utils.toArray(".slide-img12").forEach((img, i) => {
+      gsap.set(img, {
+        scale: lerp(0, 1, progress),
+        x: `${lerp(-120 + i, 40, progress)}vw`,
+        y: `${lerp(-450 + i, 40, progress)}vh`
       })
     })
   }
