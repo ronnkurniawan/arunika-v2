bd03391a14ecf2259b5aa7ede4e8e8eee7aac892
ronnkurniawan <ronnkurniawan@gmail.com>
2025-07-11 15:07:27 +0700

    service section desktop & mobile

--- src/services.js
+++ src/services.js
@@ -9,6 +9,7 @@ function lerp (start, end, p) {
   return start + (end - start) *  p
 }
 
+/*
 const serviceCard1 = gsap.utils.toArray(".service1");
 const serviceCard2 = gsap.utils.toArray(".service2");
 const serviceCard3 = gsap.utils.toArray(".service3");
@@ -50,4 +51,60 @@ ScrollTrigger.create({
     })
 
   }
+});
+*/
+
+// Pastikan fungsi lerp tersedia
+
+
+const serviceCard1 = gsap.utils.toArray(".service1");
+const serviceCard2 = gsap.utils.toArray(".service2");
+const serviceCard3 = gsap.utils.toArray(".service3");
+const serviceCard4 = gsap.utils.toArray(".service4");
+
+ScrollTrigger.matchMedia({
+
+  //Desktop: width ≥ 768px
+  "(min-width: 768px)": function () {
+    ScrollTrigger.create({
+      trigger: "#services",
+      start: "top bottom",
+      end: "top top",
+      scrub: true,
+      markers: false,
+      pin: false,
+      pinSpacing: true,
+      onUpdate: (self) => {
+        const progress = self.progress;
+
+        gsap.set(serviceCard1, { y: `${lerp(180, 1, progress)}vh` });
+        gsap.set(serviceCard2, { y: `${lerp(400, -23, progress)}vh` });
+        gsap.set(serviceCard3, { y: `${lerp(600, -45, progress)}vh` });
+        gsap.set(serviceCard4, { y: `${lerp(800, -65, progress)}vh` });
+      },
+    });
+  },
+
+  // Mobile: width < 768px
+  "(max-width: 767px)": function () {
+    ScrollTrigger.create({
+      trigger: "#services",
+      start: "top bottom",
+      end: "top top",
+      scrub: true,
+      markers: false,
+      pin: false,
+      pinSpacing: true,
+      onUpdate: (self) => {
+        const progress = self.progress;
+
+        gsap.set(serviceCard1, { y: `${lerp(100, 0, progress)}vh` });
+        gsap.set(serviceCard2, { y: `${lerp(250, -16, progress)}vh` });
+        gsap.set(serviceCard3, { y: `${lerp(400, -48, progress)}vh` });
+        gsap.set(serviceCard4, { y: `${lerp(550, -80, progress)}vh` });
+      },
+    });
+  },
+
+
 });
